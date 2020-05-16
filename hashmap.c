94a0eef7e8e7f0e5d6de1828ea67430dff626058
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-16 15:37:43 +0800

    push
    push

--- hashmap.c
+++ hashmap.c
@@ -134,9 +134,9 @@ struct hash_map* hash_map_new(size_t (*hash)(void*), int (*cmp)(void*,void*),
         return NULL;
     }
     hashmap_t *hashmap = malloc(sizeof(struct hash_map));
-    hashmap->buckets = malloc(sizeof(linked_list_t*) * 30000);
+    hashmap->buckets = malloc(sizeof(linked_list_t*) * 300);
     hashmap->size = 0;
-    hashmap->capacity = 30000;
+    hashmap->capacity = 300;
     for (int i = 0; i < hashmap->capacity; i++) {
         hashmap->buckets[i] = NULL;
     }
@@ -152,12 +152,12 @@ void rehash(hashmap_t* map){
     for (int i = 0; i < old_capacity; i++){
         pthread_mutex_lock(&map->buckets[i]->list_lock);
     }
-    linked_list_t** new_bucket = malloc(sizeof(linked_list_t*) * map->capacity * 2);
+    linked_list_t** new_bucket = malloc(sizeof(linked_list_t*) * map->capacity * 30000);
     linked_list_t** old_bucket = map->buckets;
+    map->capacity = map->capacity * 30000;
     for (int i = 0; i < map->capacity; i++) {
        new_bucket[i] = NULL;
     }
-    map->capacity = map->capacity * 2;
     map->buckets = new_bucket;
     for (int i = 0; i < map->capacity; i++){
         node_t* cur = old_bucket[i]->head->next;
