d3abf6b8b47e1319ae5d757aec1501205c6ccaf3
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-16 18:55:56 +0800

    push
    push

--- hashmap.c
+++ hashmap.c
@@ -194,7 +194,7 @@ void rehash(hashmap_t* map){
 
 void hash_map_put_entry_move(struct hash_map* map, void* k, void* v) {
     while (lock_rehash == 1) {
-        sleep(1);
+        usleep(1000);
     }
     if (map->size >= map->capacity){
         lock_rehash = 1;
@@ -217,7 +217,7 @@ void hash_map_put_entry_move(struct hash_map* map, void* k, void* v) {
 
 void hash_map_remove_entry(struct hash_map* map, void* k) {
     while (lock_rehash == 1) {
-        sleep(1);
+        usleep(1000);
     }
     size_t index = map->hash(k);
     index = compression(map, index);
@@ -230,7 +230,7 @@ void hash_map_remove_entry(struct hash_map* map, void* k) {
 }
 void* hash_map_get_value_ref(struct hash_map* map, void* k) {
     while (lock_rehash == 1) {
-        sleep(1);
+        usleep(1000);
     }
     size_t index = map->hash(k);
     index = compression(map, index);
