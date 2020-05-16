720f6815cfe026600493825b8b37ba941df29908
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-16 14:26:44 +0800

    push
    push

--- hashmap.c
+++ hashmap.c
@@ -69,7 +69,9 @@ void linked_list_insert(hashmap_t* map,linked_list_t* list, void* k, void* v){
     } else{
 //        printf("here\n");
         map->value_destruct(p->cur->d->value);
+        map->key_destruct(p->cur->d->k);
         p->cur->d->value = v;
+        p->cur->d->k = k;
 //        pthread_mutex_unlock(&p->cur->lock);
 //        pthread_mutex_unlock(&p->prev->lock);
     }
