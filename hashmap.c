1d38c0773b7df096d9748bed6106b2644aac932f
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-17 16:04:54 +0800

    push
    push

--- hashmap.c
+++ hashmap.c
@@ -100,10 +100,10 @@ struct hash_map* hash_map_new(size_t (*hash)(void*), int (*cmp)(void*,void*),
     }
     hashmap_t *hashmap = malloc(sizeof(struct hash_map));
     pthread_mutex_init(&hashmap->lock, NULL);
-    hashmap->buckets = malloc(sizeof(linked_list_t*) * 16);
+    hashmap->buckets = malloc(sizeof(linked_list_t*) * 50);
 //    array = malloc(sizeof(pthread_mutex_t) * 1000);
     hashmap->size = 0;
-    hashmap->capacity = 16;
+    hashmap->capacity = 50;
     for (int i = 0; i < hashmap->capacity; i++) {
         hashmap->buckets[i] = NULL;
     }
